70ad471311adca319ffbeea0d127a96b190195b2
MR5121472 <177574924+MR5121472@users.noreply.github.com>
2025-06-18 01:41:19 +0500

    Create telegram_handler.py

---
+++ telegram_handler.py
@@ -0,0 +1,12 @@
+import requests
+
+BOT_TOKEN = "7901711799:AAEWnySjRO5KgEMpUQmz7fwnYzlumt_AlX4"
+CHAT_ID = "6908281054"
+
+def send_telegram_info(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    requests.post(url, data=data)
