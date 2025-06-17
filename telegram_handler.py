e8af79c3dcc53e771555ddf06acc776f915d1228
MR5121472 <177574924+MR5121472@users.noreply.github.com>
2025-06-18 01:53:17 +0500

    Update telegram_handler.py

--- telegram_handler.py
+++ telegram_handler.py
@@ -1,9 +1,10 @@
+# 📁 FaizanCloudTrap/telegram_handler.py
 import requests
 
 BOT_TOKEN = "7901711799:AAEWnySjRO5KgEMpUQmz7fwnYzlumt_AlX4"
 CHAT_ID = "6908281054"
 
-def send_telegram_info(message):
+def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {
         "chat_id": CHAT_ID,
